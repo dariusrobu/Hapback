1fcf6353e6cd1a8e8392a116c844828a92de3632
Robu Darius <robu.i.darius@gmail.com>
2026-01-31 01:00:00 +0200

    test(playback): Add basic tests for PlaybackManager

---
+++ Hapback/HapbackTests/PlaybackTests.swift
@@ -0,0 +1,19 @@
+//
+//  PlaybackTests.swift
+//  HapbackTests
+//
+//  Created by Conductor on 30.01.2026.
+//
+
+import XCTest
+@testable import Hapback
+
+@MainActor
+final class PlaybackTests: XCTestCase {
+
+    func testPlaybackManagerInitialization() {
+        let manager = PlaybackManager.shared
+        XCTAssertNotNil(manager)
+        XCTAssertFalse(manager.isPlaying)
+    }
+}
